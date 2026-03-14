fa0fe5bda8ae4fd5cd0b042f892ff12085241d11
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-03-14 03:09:03 +0000

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 window.__ENV__ = {
   SUPABASE_URL: "https://xmmqhahsfhlmplbuchat.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtbXFoYWhzZmhsbXBsYnVjaGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNDQxMTQsImV4cCI6MjA4MTcyMDExNH0.vHIu1A2uMw4d9eFFyOj98lvVOppC8msdMCbS1Oj-lAM",
-FREE_AI_URL:"https://billy-free-ai-production.up.railway.app",
+FREE_AI_URL:"https://billy-free-ai-production.up.railway.app/",
   RECAPTCHA_SITE_KEY:"6LeY4V4sAAAAAOCau8QNv_u1vynCqNripJK_SmuC"
 };
