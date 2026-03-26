32b7050be4f6a7f64792baa2e643ce0166c27263
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-03-26 19:17:55 +0000

    Update env.js

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ window.__ENV__ = {
   SUPABASE_URL: "https://acbnqeeukfxewiqswgbw.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjYm5xZWV1a2Z4ZXdpcXN3Z2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMDY5MzIsImV4cCI6MjA4OTg4MjkzMn0.bwkZs0yqNAooAh6cV16dcwZwNz_5H5xvAu7eiQtR4wE",
   FREE_AI_URL:"https://billy-free-ai-production.up.railway.app",
-  RECAPTCHA_SITE_KEY:"6LeY4V4sAAAAAOCau8QNv_u1vynCqNripJK_SmuC",
+  RECAPTCHA_SITE_KEY:"6LfKh5ksAAAAAAzD0RSUx_l6OscW90MKZfiin22z",
   ONESIGNAL_APP_ID: "7fad2e88-14b3-42b4-84f6-19d924af5b1e",
   APP_NAME: "HeloxAi"
 };
