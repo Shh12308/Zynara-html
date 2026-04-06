1aeb1b98bc37fd70a3d50b98d341286d18aa3390
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-04-06 23:18:48 +0100

    Update env.js

--- env.js
+++ env.js
@@ -3,6 +3,7 @@ window.__ENV__ = {
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjYm5xZWV1a2Z4ZXdpcXN3Z2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMDY5MzIsImV4cCI6MjA4OTg4MjkzMn0.bwkZs0yqNAooAh6cV16dcwZwNz_5H5xvAu7eiQtR4wE",
   FREE_AI_URL:"https://billy-free-ai-production.up.railway.app",
   TURNSTILE_SITE_KEY:"0x4AAAAAACxvfQw84rEK9JtR",
+  BACKEND_URL:"https://vidmint-withdraw-production.up.railway.app",
   STRIPE_PAYMENT_LINK:"https://buy.stripe.com/eVq4gzbXL8Srdig0oB53O06",
   RECAPTCHA_SITE_KEY:"6Le6lZksAAAAAJrbiMWvHD4zUC3lHiNFCTLeAJgC",
   ONESIGNAL_APP_ID: "7fad2e88-14b3-42b4-84f6-19d924af5b1e",
